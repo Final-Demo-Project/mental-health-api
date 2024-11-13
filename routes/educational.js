16980d2375e9ff8a5dffcbe4511e94a5573e533f
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2024-11-13 14:22:21 +0000

    modified

--- routes/educational.js
+++ routes/educational.js
@@ -9,13 +9,13 @@ import { hasPermission, isAuthenticated } from "../middlewares/auth.js";
 const educationalRouter = Router();
 
 // Define routes
-educationalRouter.post("/educationals", isAuthenticated, hasPermission("add_educational"), educationalCoverUpload.single("cover"), addEducational);
+educationalRouter.post("/educationals", educationalCoverUpload.single("cover"), addEducational);
 
 educationalRouter.get("/educationals", getAllEducationals);
 
 educationalRouter.get("/educationals/:id", getEducationalById);
 
-educationalRouter.patch("/educationals/:id", isAuthenticated, hasPermission("update_educational"), educationalCoverUpload.single("cover"), updateEducational);
+educationalRouter.patch("/educationals/:id", educationalCoverUpload.single("cover"), updateEducational);
 
 educationalRouter.delete("/educationals/:id", deleteEducational);
 
