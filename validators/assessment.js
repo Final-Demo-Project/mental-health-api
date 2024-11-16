8483bc3c91c05b66d738a72fa2e899ae0a664697
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2024-11-16 07:32:10 +0000

    modified

--- validators/assessment.js
+++ validators/assessment.js
@@ -2,5 +2,6 @@ import Joi from "joi";
 
 export const takeAssessmentValidator = Joi.object({
    responses: Joi.object().required(),
-   date: Joi.date().required()
+   date: Joi.date().required(),
+   score: Joi.number().required()
 })
