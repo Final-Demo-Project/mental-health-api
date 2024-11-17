81f9d2a306714904970b0525135337e78df64aae
Francis Kojo Amekeh <franciskojoamekeh@gmail.com>
2024-11-17 08:01:28 +0000

    modified

--- models/assessment.js
+++ models/assessment.js
@@ -3,10 +3,22 @@ import { toJSON } from "@reis/mongoose-to-json";
 
 const assessmentSchema = new Schema({
     user: { type: Types.ObjectId, ref: 'User' },
-   type: String, // e.g., "PHQ-9"
-   score: Number, // Total score of the quiz
-   responses: {type: Object}, // Array of answers to individual quiz questions
-   date: { type: Date, default: Date.now }
+    type: { type: String, required: true }, // e.g., "PHQ-9"
+    score: { type: Number, required: true }, // Total score of the quiz
+    responses: {
+        anhedonia: { type: Number, required: true },
+        depressedMood: { type: Number, required: true },
+        sleepIssues: { type: Number, required: true },
+        fatigue: { type: Number, required: true },
+        appetite: { type: Number, required: true },
+        selfFailure: { type: Number, required: true },
+        concentration: { type: Number, required: true },
+        movement: { type: Number, required: true },
+        suicidalThoughts: { type: Number, required: true },
+    }, // Array of answers to individual quiz questions
+    severity: {type: String, enum: ['None', 'Mild', 'Moderate', 'Moderately Severe Depression', 'Severe Depression'],
+        required: true,}, // Calculated severity level
+    date: { type: Date, default: Date.now }
 }, {
     timestamps: true
 });
--- validators/assessment.js
+++ validators/assessment.js
@@ -3,5 +3,6 @@ import Joi from "joi";
 export const takeAssessmentValidator = Joi.object({
    responses: Joi.object().required(),
    date: Joi.date().required(),
-   score: Joi.number().required()
+   score: Joi.number().required(),
+   severity: Joi.string().required()
 })
